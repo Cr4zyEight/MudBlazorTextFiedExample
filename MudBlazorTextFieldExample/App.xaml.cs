03099d23548747cb5dc62968bfafe21681804f32
Vincent Frebel <frebel@wkg-software.de>
2024-06-06 11:24:04 +0200

    init

---
+++ MudBlazorTextFieldExample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MudBlazorTextFieldExample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
